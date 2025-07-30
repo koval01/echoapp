3ca487bd94abd82a2306ef82f0cc57b9d4bf0e47
Koval Yaroslav <ksvarog2004@gmail.com>
2025-07-30 16:40:27 +0200

    Update

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,6 +1,7 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20250730_095721_create_users_table;
+mod m20250730_144004_create_tower_sessions_table;
 
 pub struct Migrator;
 
@@ -9,6 +10,7 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
             Box::new(m20250730_095721_create_users_table::Migration),
+            Box::new(m20250730_144004_create_tower_sessions_table::Migration),
         ]
     }
 }
