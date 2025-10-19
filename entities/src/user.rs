7f7faeb6d5418f597bf4fe62c4f0e1cd59588b14
Koval Yaroslav <ksvarog2004@gmail.com>
2025-10-19 21:23:32 +0200

    Update

--- entities/src/user.rs
+++ entities/src/user.rs
@@ -1,11 +1,14 @@
 use sea_orm::entity::prelude::*;
 use sea_orm::Set;
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Serialize, Deserialize)]
 #[sea_orm(table_name = "users")]
 pub struct Model {
-    #[sea_orm(primary_key, unique, auto_increment = false)]
+    #[sea_orm(primary_key, auto_increment = false)]
+    pub id: Uuid,
+    #[sea_orm(indexed, unique)]
     pub telegram_id: i64,
     pub first_name: String,
     pub last_name: Option<String>,
@@ -28,6 +31,7 @@ pub enum Relation {}
 impl ActiveModelBehavior for ActiveModel {
     fn new() -> Self {
         let mut active_model = <Self as ActiveModelTrait>::default();
+        active_model.id = Set(Uuid::new_v4());
         active_model.allows_write_to_pm = Set(true);
         active_model.language_code = Set("en".to_owned());
         active_model.is_admin = Set(false);
--- migration/src/m20250730_095721_create_users_table.rs
+++ migration/src/m20250730_095721_create_users_table.rs
@@ -18,10 +18,16 @@ impl MigrationTrait for Migration {
                     .table(User::Table)
                     .if_not_exists()
                     .col(
+                        ColumnDef::new(User::Id)
+                            .uuid()
+                            .not_null()
+                            .primary_key(),
+                    )
+                    .col(
                         ColumnDef::new(User::TelegramId)
                             .big_integer()
                             .not_null()
-                            .primary_key(),
+                            .unique_key(),
                     )
                     .col(ColumnDef::new(User::FirstName).string().not_null())
                     .col(ColumnDef::new(User::LastName).string())
@@ -61,6 +67,18 @@ impl MigrationTrait for Migration {
             )
             .await?;
 
+        // Create indexes separately
+        manager
+            .create_index(
+                Index::create()
+                    .name("idx_users_telegram_id")
+                    .table(User::Table)
+                    .col(User::TelegramId)
+                    .unique()
+                    .to_owned(),
+            )
+            .await?;
+
         manager
             .create_index(
                 Index::create()
@@ -73,6 +91,16 @@ impl MigrationTrait for Migration {
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Drop indexes first
+        manager
+            .drop_index(
+                Index::drop()
+                    .name("idx_users_telegram_id")
+                    .table(User::Table)
+                    .to_owned(),
+            )
+            .await?;
+
         manager
             .drop_index(
                 Index::drop()
@@ -93,6 +121,7 @@ impl MigrationTrait for Migration {
 enum User {
     #[sea_orm(iden = "users")]
     Table,
+    Id,
     TelegramId,
     FirstName,
     LastName,
