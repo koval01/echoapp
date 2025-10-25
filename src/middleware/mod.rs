e3d5f238aeeb82e36b47a982c0f4377be6c76b93
Koval Yaroslav <ksvarog2004@gmail.com>
2025-10-25 19:44:00 +0200

    Rename jwt middleware to session

--- src/middleware/mod.rs
+++ src/middleware/mod.rs
@@ -2,10 +2,10 @@ mod request_id;
 mod process_time;
 mod sync_user;
 mod initdata;
-mod jwt;
+mod session;
 
 pub use request_id::request_id_middleware;
 pub use process_time::process_time_middleware;
 pub use initdata::validate_initdata_middleware;
-pub use jwt::validate_jwt_middleware;
+pub use session::validate_jwt_middleware;
 pub use sync_user::sync_user_middleware;
